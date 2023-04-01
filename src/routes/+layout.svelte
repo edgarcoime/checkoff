40f7c0f5a7502c196f0d8c4ecb2d877d99e4218f
Edgar Coime <59620774+edgarcoime@users.noreply.github.com>
2023-04-01 02:33:52 +0000

    feat: setup basic login ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,8 @@
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.700);
+    background-color: theme(colors.gray.100);
   }
 </style>
-
-<nav>
-	<a href="/">home</a>
-	<a href="/account">account</a>
-	<a href="/app">app</a>
-</nav>
 
 <slot />
